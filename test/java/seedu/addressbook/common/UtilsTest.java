0c3c5ec14ee7ad4279917eefa496c724ed3bc7b5
Ronghua <ronghualui@gmail.com>
2016-09-06 23:34:54 +0800

    Added JUnit test testOneNull

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -11,4 +11,9 @@ public class UtilsTest {
 	public void testNoNull() {
 		assertFalse(Utils.isAnyNull("There", "are", "no", "nulls", 1, 5, false));
 	}
+	
+	@Test
+	public void testOneNull() {
+		assertTrue(Utils.isAnyNull("There", "is", "one", null, 1, 5, true));
+	}
 }
