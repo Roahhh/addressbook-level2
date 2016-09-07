026f2e05c639fd89765373cc5d930e08e1717602
Ronghua <ronghualui@gmail.com>
2016-09-07 09:09:41 +0800

    Added JUnit test testSameName

---
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -0,0 +1,18 @@
+package seedu.addressbook.data.person;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class NameTest {
+
+	@Test
+	public void testSameName() throws IllegalValueException {
+		Name a = new Name("Test");
+		Name b = new Name("Test");	
+		
+		assertTrue(a.isSimilar(b));
+	}
+}
